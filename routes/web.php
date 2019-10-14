5fca1693f2cbf7f35ce8a4d647b71a00a5083b00
Pedro Porras <pporrasm@miumg.edu.gt>
2019-10-14 13:48:14 -0600

    Rutas con parametros

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,25 @@ Route::get('/usuarios', function () {
     return 'Hola usuario' ;
 });
 
-Route::get('/usuarios/{id}', function ($id) {
-    return 'Mostrando detalle del usuario '.$id;
-})->where('id', '[0-9]+');
-
 Route::get('/usuarios/nuevo', function (){
     return "Nuevo usuario";
 });
+
+Route::get('/usuarios/{id}', function ($id) {
+    return 'Mostrando detalle del usuario '.$id;
+});
+//->where('id', '[0-9]+'); Para parametros numericos
+//->where('id', '\d'); Para parametros numericos
+//->where('id', '\w'); Para parametros con numero y letras
+
+//Agregar signo de interragación en el parametro que quiera que sea opcional
+Route::get('/saludo/{name}/{nickname?}', function ($name,$nickname=null){
+    if ($nickname != null){
+    return "Hola {$name}, tu apodo es {$nickname}";   
+    } else{
+        return "Hola {$name}, no tienes apodo";
+
+    }
+});
+
 ?>
