9f52f20c0427d605b0b0c5210c0dbe4a37883f00
Pedro Porras <pporrasm@miumg.edu.gt>
2019-10-14 13:51:08 -0600

    Rutas con valores opciones

--- routes/web.php
+++ routes/web.php
@@ -22,13 +22,19 @@ Route::get('/usuarios/{id}', function ($id) {
 //->where('id', '\w'); Para parametros con numero y letras
 
 //Agregar signo de interragación en el parametro que quiera que sea opcional
-Route::get('/saludo/{name}/{nickname?}', function ($name,$nickname=null){
-    if ($nickname != null){
-    return "Hola {$name}, tu apodo es {$nickname}";   
-    } else{
-        return "Hola {$name}, no tienes apodo";
-
+//Los valores son opcioens o valores por defecto que se colocan en la funcion
+Route::get('/saludo/{name?}/{nickname?}', function ($name = null,$nickname=null){
+    if ($name == null){
+        return "No se encontraron valores";
+    } else {
+        if ($nickname != null){
+            return "Hola {$name}, tu apodo es {$nickname}";   
+            } else{
+                return "Hola {$name}, no tienes apodo";
+        
+            }
     }
+    
 });
 
 ?>
