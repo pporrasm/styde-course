31770e782173b76dc052ed28bc6cc6eb7b2a3f83
Pedro Porras <pporrasm@miumg.edu.gt>
2019-10-14 13:26:46 -0600

    Ruta para crear enviar Id

--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,8 @@ Route::get('/usuarios', function () {
     return 'Hola usuario' ;
 });
 
+Route::get('/usuarios/detalle/{id}', function ($id) {
+    return 'Mostrando detalle del usuario '.$id;
+});
 
 ?>
