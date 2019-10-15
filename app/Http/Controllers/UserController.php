f4d3ea6e3ec55ebceb857c67e5baedfa5fb21027
Pedro Porras <pporrasm@miumg.edu.gt>
2019-10-15 16:03:26 -0600

    Creación y uso de vistas

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,32 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function index(){
-        return 'Hola usuario' ;
+        //uso de arreglo estatico
+        $users = [
+            'Juan',
+            'Pedro',
+            'Jose',
+            'Julian',
+            '<script>alert("CLicker")</script>'
+        ];
+        $title = "Listado de usuarios!";
+
+        //dd () Helper de debug de laravel similar al var_dump
+        //dd(compact('users','title'));
+
+        //return 'Hola usuario' ;
+        /**return view('users', [
+            'users'=>$users,
+            'title'=> 'Listado de Usuarios!'
+        ]);  Envio de variables implicitamente**/
+        /**return view('users')->with([
+            'users'=>$users,
+            'title'=> 'Listado de Usuarios!' 
+        ]); Envio con el argunto with**/
+        //return view('users')->with('users', $users)->with('title', 'Listado de Usuarios!');
+   
+
+        return view('users', compact('users','title'));
     }
 
     public function show($id){
